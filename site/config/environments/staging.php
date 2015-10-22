af828878f44c21dcf1c6341c2ffe481e7a1b3813
Corey <coreybruyere@ymail.com>
2015-10-21 21:24:07 -0700

    update staging deploy

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -1,7 +1,6 @@
 <?php
 /* Staging */
 ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', true);
-define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+// define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
