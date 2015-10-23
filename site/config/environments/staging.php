406721f6b51b12b70f7882bcc54b8209de5297b2
Corey <coreybruyere@ymail.com>
2015-10-22 18:56:41 -0700

    change deploy settings

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -4,4 +4,4 @@ ini_set('display_errors', 1);
 define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', true);
 define('SCRIPT_DEBUG', true);
-// define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
