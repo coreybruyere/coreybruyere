b4a93094ea4680d693bd914fcebc38b499666e60
Corey <coreybruyere@ymail.com>
2015-10-29 22:26:52 -0700

    add 404 image

--- site/web/app/themes/coreybruyere/404.php
+++ site/web/app/themes/coreybruyere/404.php
@@ -1,17 +1,18 @@
+<section class="section">
+  <?php
+    // Hero Block
+    get_template_part('templates/hero', get_post_type());
+  ?>
+</section>
 
-
-
-<?php
-  // Hero Block
-  get_template_part('templates/hero', get_post_type());
-?>
-<div class="l-container">
-
-  <div class="l-block l-block--top">
-    <div class="alert alert--error">
-      <?php _e('Sorry, no results were found.', 'sage'); ?>
+<section class="section">
+  <div class="l-container">
+    <div class="l-block l-block--top">
+      <img class="l-figure l-block" src="<?php echo get_bloginfo('template_directory');?>/assets/images/Milhouse.png">
+      <div class="alert alert--error">
+        <?php _e('Sorry, no results were found.', 'sage'); ?>
+      </div>
+      <?php get_search_form(); ?>
     </div>
-    <?php get_search_form(); ?>
   </div>
-
-</div>
+</section>
